f2241a09b0f9c996b3d999a29f4f427ee9537e7a
varenyeolad <97789571+varenyeolad@users.noreply.github.com>
2022-05-11 01:35:38 +0600

    Procfile

--- java.js
+++ java.js
@@ -5,7 +5,7 @@ const express =require('express')
 const mongoose = require('mongoose')
 const { createServer } = require('http')
 
-const port=3000;
+const port= process.env.PORT || 3000;
 
 /* for to validate and authentication*/
 const passport = require('passport')
